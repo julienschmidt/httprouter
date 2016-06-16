96af8a1e86aef229a62c4c06659371276697c298
Travis J Parker <travis.parker@gmail.com>
2016-06-16 10:15:21 -0700

    Supports Params in net/http Handlers with go1.7+
    Go 1.7 introduces request-scoped context, providing a place to hang
    Params even in net/http standard Handlers attached with Router.Handler
    or Router.HandlerFunc.
    
    This provides an alternative implementation of Router.Handler which
    attaches the params to the request context. It also includes a bit of
    new public API: ParamsKey is the context key used, and
    ParamsFromContext uses ParamsKey to get Params from a context.Context.
    
    All of this is guarded by a "go1.7" build tag. With go 1.6 and older
    none of the new API is available and Params continue to be thrown away
    in Handler/HandlerFunc-attached endpoints.

---
+++ stdparams.go
@@ -0,0 +1,36 @@
+// +build go1.7
+
+package httprouter
+
+import (
+	"context"
+	"net/http"
+)
+
+// ParamsKey is the request context key under which URL params are stored.
+//
+// This is only present from go 1.7.
+const ParamsKey = "_httprouter_params"
+
+// Handler is an adapter which allows the usage of an http.Handler as a
+// request handle. With go 1.7+, the Params will be available in the
+// request context under ParamsKey.
+func (r *Router) Handler(method, path string, handler http.Handler) {
+	r.Handle(method, path,
+		func(w http.ResponseWriter, req *http.Request, p Params) {
+			ctx := req.Context()
+			ctx = context.WithContext(ctx, ParamsKey, p)
+			req = req.WithContext(ctx)
+			handler.ServeHTTP(w, req)
+		},
+	)
+}
+
+// ParamsFromContext pulls the URL parameters from a request context,
+// or returns nil if none are present.
+//
+// This is only present from go 1.7.
+func ParamsFromContext(ctx context.Context) Params {
+	p, _ := ctx.Value(ParamsKey).(Params)
+	return p
+}
