b7b2354adbc014803565318c359b330c5efc72f0
eaglerayp <eagle_rayp@hotmail.com>
2016-10-14 15:17:41 +0800

    feature: Add query args for redirect

--- examples/basic/basic.go
+++ examples/basic/basic.go
@@ -23,11 +23,20 @@ func MultiParams(ctx *fasthttp.RequestCtx) {
 	fmt.Fprintf(ctx, "hi, %s, %s!\n", ctx.UserValue("name"), ctx.UserValue("word"))
 }
 
+// QueryArgs is used for uri query args test #11:
+// if the req uri is /ping?name=foo, output: Pong! foo
+// if the req uri is /piNg?name=foo, redirect to /ping, output: Pong!
+func QueryArgs(ctx *fasthttp.RequestCtx) {
+	name := ctx.QueryArgs().Peek("name")
+	fmt.Fprintf(ctx, "Pong! %s\n", string(name))
+}
+
 func main() {
 	router := fasthttprouter.New()
 	router.GET("/", Index)
 	router.GET("/hello/:name", Hello)
 	router.GET("/multi/:name/:word", MultiParams)
+	router.GET("/ping", QueryArgs)
 
 	log.Fatal(fasthttp.ListenAndServe(":8080", router.Handler))
 }
